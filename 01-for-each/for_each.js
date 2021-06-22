a762101ebb779b6a09a7dc4abac059aec0e7736d
eyosiyas <eyosiyas@AULA1D-DOCENTE.edu-il.it>
2021-06-22 15:18:01 +0200

    ex,for_each done

--- 01-for-each/for_each.js
+++ 01-for-each/for_each.js
@@ -1,5 +1,8 @@
 // define an array of fruits
-
-// .forEach((fruit, index) => {
-    // console.log() a message interpolating the name of the fruit and it's position in the array (index increased by 1);
+const fruits =
+    [`apple`, `banana`, `orange`, `Apricot`, `Avocado`, `Blackberry`, `Cantaloupe`];
+{
+    fruits.forEach(Element => console.log(Element));
+}
+// console.log() a message interpolating the name of the fruit and it's position in the array (index increased by 1);
 // })
