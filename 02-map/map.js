9d4f24c96669d916c932830c430d55f789ac235a
eyosiyas <eyosiyas@AULA1D-DOCENTE.edu-il.it>
2021-06-23 10:38:19 +0200

    ex.map done

--- 02-map/map.js
+++ 02-map/map.js
@@ -1,13 +1,20 @@
+
 const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 
-const secretMessage = // use .map to get the first letter of every animal name in the arrray animals
+// use .map to get the first letter of every animal name in the arrray animalsc
+const secretMessage = animals.map(firstLetter => {
+    return firstLetter[0]
+});
 
-console.log(secretMessage.join(''));
+console.log(secretMessage);
+var firstLetterjoin = secretMessage.join(``);
+console.log(firstLetterjoin);
 
-
-const bigNumbers = [100, 200, 300, 400, 500];
 
 // Create the smallNumbers array below with map
 
-
+const bigNumbers = [100, 200, 300, 400, 500];
+const smallNumbers = bigNumbers.map(bigNumbers => {
+    return bigNumbers / 100;
+});
 console.log(smallNumbers);
