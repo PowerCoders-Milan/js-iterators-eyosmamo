91a1e8e5b0f478bd2c0f4c1441f0e4c314ab2e12
eyosiyas <eyosiyas@AULA1D-DOCENTE.edu-il.it>
2021-06-23 11:19:39 +0200

    ex.filter done

--- 03-filter/filter.js
+++ 03-filter/filter.js
@@ -1,7 +1,27 @@
 // create a randomNumbers array, containing 100 random numbers
+const randomNumbers = [];
+for (i = 0; i < 100; i++) {
+    randomNumbers[i] = Math.floor(Math.random() * 100) + 1;
+} console.log(randomNumbers);
 
 // create a function isPrime(number)
+function isPrime(n) {
+
+    if (n === 1) {
+        return false;
+    }
+    else if (n === 2) {
+        return true;
+    } else {
+        for (var i = 2; i < n; i++) {
+            if (n % i === 0) {
+                return false;
+            }
+        }
+        return true
+    }
+}
 
 // create a primeNumbers array
-const primeNumbers = // filter randomNumbers with isPrime(number)
+const primeNumbers = randomNumbers.filter(isPrime); // filter randomNumbers with isPrime(number)
 console.log(primeNumbers)
